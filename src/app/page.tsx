b52e2d483ecffee40faa8403ad39a8faff02e8a1
aghiasi <ghiasikhamene@gmail.com>
2025-01-17 05:41:21 -0800

    finishing header part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+import Img from "../../public/assets/images/93682279.jpg";
 import Image from "next/image";
-
+import * as motion from "motion/react-client";
+import GitHub from "./components/GitHub";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
-    </div>
+    <>
+      <header className="header">
+        <div className="header_wraper">
+          <h1 className="mb-16">Wellcome To Aghiasi WebSite .</h1>
+        </div>
+        <motion.div initial={{opacity:0}} animate={{opacity:1}}>
+          <Image
+            className="header_img"
+            src={Img}
+            width={200}
+            height={200}
+            alt=""
+          />
+          <a href="https://github.com/aghiasi"className="header_img header_img-icon"><GitHub /></a>
+        </motion.div>
+      </header>
+    </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,7 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+      }
   },
   plugins: [],
 } satisfies Config;
