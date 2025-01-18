b9743da72b6838a751c72aee8784962bcd1f02cc
aghiasi <ghiasikhamene@gmail.com>
2025-01-18 11:14:33 -0800

    some changes in top sectgion

--- src/app/components/TopSection.tsx
+++ src/app/components/TopSection.tsx
@@ -2,10 +2,25 @@ import { section as Section, div as Div } from "motion/react-client";
 import CodeIcon from "@mui/icons-material/Code";
 import Diversity3Icon from "@mui/icons-material/Diversity3";
 import Diversity1Icon from "@mui/icons-material/Diversity1";
+import type { Variants } from "motion/react";
+const cardVariants: Variants = {
+    offscreen: {
+        y: 300,
+    },
+    onscreen: {
+        y: 50,
+        rotate: -10,
+        transition: {
+            type: "spring",
+            bounce: 0.4,
+            duration: 0.8,
+        },
+    },
+}
 export default function TopSection() {
   return (
     <Section className="top_section">
-      <Div>
+      <Div initial="offscreen" whileInView="onscreen" viewport={{amount:0.8}}>
         <CodeIcon sx={{ color: "white", width: "300px", height: "250px" }} />
         <p>Love to Code</p>
       </Div>
