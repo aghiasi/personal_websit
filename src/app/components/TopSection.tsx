77c8cdd65f496deb4ddc7cedc587a21153be8a64
aghiasi <ghiasikhamene@gmail.com>
2025-01-18 11:24:23 -0800

    fixing bug

--- src/app/components/TopSection.tsx
+++ src/app/components/TopSection.tsx
@@ -2,35 +2,20 @@ import { section as Section, div as Div } from "motion/react-client";
 import CodeIcon from "@mui/icons-material/Code";
 import Diversity3Icon from "@mui/icons-material/Diversity3";
 import Diversity1Icon from "@mui/icons-material/Diversity1";
-import type { Variants } from "motion/react";
-const cardVariants: Variants = {
-    offscreen: {
-        y: 300,
-    },
-    onscreen: {
-        y: 50,
-        rotate: -10,
-        transition: {
-            type: "spring",
-            bounce: 0.4,
-            duration: 0.8,
-        },
-    },
-}
 export default function TopSection() {
   return (
     <Section className="top_section">
-      <Div initial="offscreen" whileInView="onscreen" viewport={{amount:0.8}}>
+      <Div initial={{opacity:0}} whileInView={{opacity:1}}>
         <CodeIcon sx={{ color: "white", width: "300px", height: "250px" }} />
         <p>Love to Code</p>
       </Div>
-      <Div>
+      <Div initial="offscreen" whileInView="onscreen">
         <Diversity3Icon
           sx={{ color: "white", width: "300px", height: "250px" }}
         />
         <p>Hard Worker and Good TeamMate</p>
       </Div>
-      <Div>
+      <Div initial="offscreen" whileInView="onscreen">
         <Diversity1Icon
           sx={{ color: "white", width: "300px", height: "250px" }}
         />
