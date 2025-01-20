ddd693058ae14a3549c6c63af4751ca5f1b2d4db
aghiasi <ghiasikhamene@gmail.com>
2025-01-20 08:56:34 -0800

    adding random colors

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,6 +1,7 @@
 import { div as Div } from "motion/react-client";
 import Image from "next/image";
 import type { Variants } from "motion/react"
+import gradient from "random-gradient";
 const cardVariants: Variants = {
     offscreen: {
         y: 300,
@@ -17,16 +18,22 @@ const cardVariants: Variants = {
     
 }
 export default function Card(prop:any) {
+  const randomNumberGenerator =()=>{
+    const colors = ["green","blue","gray","cyan","white","purple","red","orange","yellow","fuchsia","teal"]
+   const  randomNumb =  Math.floor(Math.random()*10)
+    const random =   gradient(colors[randomNumb])
+    return random ;
+  }
   return (
     <>
-    <div className="component">
+    <div className="component ">
       <Div
         className="cardContainer relative"
         initial="offscreen"
         whileInView="onscreen"
         viewport={{ amount: 0.8 }}
       >
-        <div className="splash" />
+        <div className="splash" style={{background:randomNumberGenerator()}} />
         <Div variants={cardVariants} whileHover={{scale:1.1,rotate:2}} whileTap={{y:300}}  className="card z-[4] hover:rotate-6"><Image src={prop.img} alt="" width={200} height={200} /> </Div>
       </Div>
     </div>
