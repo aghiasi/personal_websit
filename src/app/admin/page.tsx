4d5ff90d100597a74a7e95c0402494bb8b3d74a6
aghiasi <ghiasikhamene@gmail.com>
2025-08-02 17:48:54 +0330

    adding the contact api and a semi data base for the messages

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,21 +1,47 @@
 import Link from "next/link";
 import ActiveRooms from "./components/ActiveRooms";
-export default function page() {  
+const getMails = async () => {
+  try {
+    const request = await fetch("http://localhost:3000/api/mail");
+    if (request.ok) {
+      return await request.json();
+    }
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+};
+export default async function page() {
+  const request = await getMails();
   return (
     <section className="grid md:grid-cols-7 gap-5  grid-cols-1">
       <Link
-        href={"/admin/inbox"} 
-        className=" col-span-3 mt-10 block  p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 lg:ml-20"  
+        href={"/admin/inbox"}
+        className="  col-span-3 mt-10 block  p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 lg:ml-20"
       >
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Inbox Messages
+          Inbox Messages
         </h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
-        </p>
+        <div className="h-[500px] overflow-y-scroll">
+          {request.message.map((items: any, index: number) => (
+            <div
+              key={index}
+              className="bg-slate-600 p-2 m-1 rounded-lg hover:bg-slate-500"
+            >
+              <p className="mb-2 font-bold tracking-tight text-gray-900 dark:text-white">
+                from : {items.email}
+              </p>
+              <p className="mb-2 font-bold tracking-tight text-gray-900 dark:text-white">
+                subject : {items.subject}
+              </p>
+              <p className="mb-2 font-bold tracking-tight text-gray-900 dark:text-white">
+                message : {items.message}
+              </p>
+            </div>
+          ))}
+        </div>
       </Link>
-      <ActiveRooms /> 
+      <ActiveRooms />
     </section>
   );
 }
