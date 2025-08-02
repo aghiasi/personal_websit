4fd5d55cf297beb9d62f7bd97613c03330b83caf
aghiasi <ghiasikhamene@gmail.com>
2025-08-02 18:11:45 +0330

    fixing some more bugs :)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,8 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars":"off",
+      "react-hooks/rules-of-hooks":"off"
     },
   }),
 ];
