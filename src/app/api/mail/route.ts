0ba281c21bac871bbd4b43b9da5e8aed97d0fd65
aghiasi <ghiasikhamene@gmail.com>
2025-08-02 18:02:45 +0330

    fixing some other bugs

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -1,10 +1,10 @@
 import { NextRequest } from "next/server";
 import { promises as fs } from "fs";
-import db from "../../../../database/database.json"
+import db from "../../../../database/database.json";
 import path from "path";
 export const GET = async (req: NextRequest) => {
-  const request = req ; 
-  console.log(db)
+  const request = req;
+  console.log(db, req);
   try {
     const filePath = path.join(process.cwd(), "database", "database.json");
     const allJson = await fs.readFile(filePath, "utf8");
@@ -19,9 +19,11 @@ export const POST = async (req: NextRequest) => {
     const filePath = path.join(process.cwd(), "database", "database.json");
     const allJson = await fs.readFile(filePath, "utf8");
     const allMessage = JSON.parse(allJson);
-     const newallMessage = [...allMessage.message, data];
-    const jsonData = await fs.writeFile(filePath, JSON.stringify(newallMessage));
-    return new Response(JSON.stringify({}), { status: 200 });
+    const newallMessage = [...allMessage.message, data];
+    fs.writeFile(filePath, JSON.stringify(newallMessage)).then((result) => {
+      console.log(result)
+      return new Response(JSON.stringify({}), { status: 200 });
+    });
   } catch (e) {
     console.log(e);
   }
