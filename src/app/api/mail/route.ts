071cbc2569fe6d7b210d0c4250ad5f89c5c3ba9b
aghiasi <ghiasikhamene@gmail.com>
2025-08-09 12:55:46 +0330

    fixing some other bugs

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -1,11 +1,7 @@
 import { NextRequest } from "next/server";
-let data = {
-  message: [{ email: "ssakdf", subject: "slhdaf", message: "ksadj;f" }],
-};
 export const GET = async (req: NextRequest) => {
   try {
-    const message = data.message;
-    return new Response(JSON.stringify(message), { status: 200 });
+    return new Response(JSON.stringify(""), { status: 200 });
   } catch (e) {
     console.log(e);
   }
@@ -13,8 +9,7 @@ export const GET = async (req: NextRequest) => {
 export const POST = async (req: NextRequest) => {
   try {
     const newData = await req.json();
-    data.message.push(newData);
-    return new Response(JSON.stringify(data.message), { status: 200 });
+    return new Response(JSON.stringify({s:""}), { status: 200 });
   } catch (e) {
     console.log(e);
   }
